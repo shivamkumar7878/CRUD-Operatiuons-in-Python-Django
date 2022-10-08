5eaed2985889988ee65b52ab25098b21d3226df2
shivam kumar <66627414+shivamkumar7878@users.noreply.github.com>
2022-10-09 00:36:54 +0530

    commit

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-l6y$!(j2l^)*f)0fj$z3i5at4k63o$4bi&$tao-uk0!n^^#)wi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -122,7 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     STATIC_DIR,
 ]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
